fac2431b0484853a12c2c6f63cb2ae633e84a129
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 14:20:23 +0530

    Added State

--- SwiftUI-HelloState/ContentView.swift
+++ SwiftUI-HelloState/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let firstname = "Arpit"
+    private let lastname = "Dixit"
+    @State private var name = "Arpit"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(name)
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .padding()
+            Button(action: {
+                self.name = name == firstname ? lastname : firstname
+            }) {
+                Text("Click Me")
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(.black)
+            }
+        }
     }
 }
 
