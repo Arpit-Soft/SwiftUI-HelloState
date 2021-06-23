d48d35909b398f5d42c4bce5bb6aa577bab2bdf0
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 14:06:32 +0530

    Initial Commit

---
+++ SwiftUI-HelloState/SwiftUI_HelloStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HelloStateApp.swift
+//  SwiftUI-HelloState
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HelloStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
